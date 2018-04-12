54b27c70137a66f5aa8d6fee561c72044b713a44
xqs002 <501919337@qq.com>
2018-04-12 17:33:58 +0800

    加载前端数据到前端

--- app/main/index.py
+++ app/main/index.py
@@ -1,8 +1,9 @@
 # coding=utf-8
 
 
-from flask import render_template, request
+from flask import render_template, request, jsonify
 from . import main
+from .. import spiders
 
 
 @main.route('/')
@@ -13,5 +14,15 @@ def index():
 @main.route('/start_spider/', methods=['GET', 'POST'])
 def start_spider():
     if request.method == 'POST':
-        urls = request.form['urls']
-        print urls
+        urls = str(request.form['urls'])
+        mid_str =  urls.split('.') 
+        if mid_str[1] == 'betburger':
+            # www.betburger.com
+            urls = 'https://api-lv.betburger.com/api/v1/arbs/pro_search?access_token='
+            dir_url = 'https://api-lv.betburger.com/api/v1/directories?access_token='
+            bet_val_url = 'https://api-lv.betburger.com/api/v1/bet_combinations/'
+            mb = spiders.back_mod_betburger.ModBetburger(urls, dir_url, bet_val_url)
+            data = mb.get_data_info()
+            return jsonify({'data': data})
+        else:
+            return jsonify({'data': False})
